be60160f7b132c62de52955f9a502c27077f76aa
Komando <167818831+komandod@users.noreply.github.com>
2024-09-12 16:27:44 +0300

    added comments for probability

--- formula_main.c
+++ formula_main.c
@@ -213,10 +213,16 @@ void populate_names(char **destination_arr, int num_of_drivers) {
 
 void generate_laptime(int **lap_times, int total_drivers, int total_laps, int *dnf_status, int *dnf_lap) {
     int dnf_random;
+    int driver_random;
     for(int i = 0; i < total_laps; i++) {
+        // Generate a random number and divide it by the number of drivers
+        // So that we basically have a specified range for each driver
         dnf_random = (rand() % 444*i) / total_drivers;
         for(int j = 0; j < total_drivers; j++) {
-            int driver_random = rand() % dnf_random + 1;
+            // Generate a random number within the specified range for a driver
+            driver_random = rand() % dnf_random + 1;
+            // If the generated number is within the 3% of that specified range 
+            // this means that the driver will get disqualified
             if(driver_random <= (dnf_random*0.03)) {
                 if(dnf_status[j] != 1) {
                     dnf_lap[j] = i;
